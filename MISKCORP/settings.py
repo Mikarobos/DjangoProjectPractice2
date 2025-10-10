857319580b7225dac0ce48b803bfd9a9ce2771f5
MIKA <boss.ibragim.magomedov.04@inbox.ru>
2025-10-10 18:16:45 +0300

    Add Login&Register, EmailAuthent, Account

--- MISKCORP/settings.py
+++ MISKCORP/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
-
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env.read_env(BASE_DIR / '.env')
 
 
 SECRET_KEY = 'django-insecure-w=34_+7n4#cjb2a5e&!z-zeduq#d)_$(7oy3@h%(^i3ez1sca3'
@@ -22,10 +25,14 @@ INSTALLED_APPS = [
     
     #third party libraries
     'mathfilters',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'django_email_verification',
 
     #apps
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -90,16 +97,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-
+#Project Inside Settinngs
 LANGUAGE_CODE = 'ru-ru'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
+#Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
@@ -107,3 +112,54 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+
+def email_verified_callback(user):
+    user.is_active = True
+
+
+# def password_change_callback(user, password):
+#     user.set_password(password)
+
+
+# Global Package Settings
+EMAIL_FROM_ADDRESS = 'boss.ibragim.magomedov.04@inbox.ru'  # mandatory
+EMAIL_PAGE_DOMAIN = 'http://127.0.0.1:8000/'  # mandatory (unless you use a custom link)
+EMAIL_MULTI_USER = False  # optional (defaults to False)
+
+# Email Verification Settings (mandatory for email sending)
+EMAIL_MAIL_SUBJECT = 'Confirm your email {{ user.username }}'
+EMAIL_MAIL_HTML = 'account/email/mail_body.html'
+EMAIL_MAIL_PLAIN = 'account/email/mail_body.txt'
+EMAIL_MAIL_TOKEN_LIFE = 60 * 60  # one hour
+
+# Email Verification Settings (mandatory for builtin view)
+EMAIL_MAIL_PAGE_TEMPLATE = 'account/email/email_success_template.html'
+EMAIL_MAIL_CALLBACK = email_verified_callback
+
+# Password Recovery Settings (mandatory for email sending)
+# EMAIL_PASSWORD_SUBJECT = 'Change your password {{ user.username }}'
+# EMAIL_PASSWORD_HTML = 'password_body.html'
+# EMAIL_PASSWORD_PLAIN = 'password_body.txt'
+# EMAIL_PASSWORD_TOKEN_LIFE = 60 * 10  # 10 minutes
+
+# Password Recovery Settings (mandatory for builtin view)
+# EMAIL_PASSWORD_PAGE_TEMPLATE = 'password_changed_template.html'
+# EMAIL_PASSWORD_CHANGE_PAGE_TEMPLATE = 'password_change_template.html'
+# EMAIL_PASSWORD_CALLBACK = password_change_callback
+
+
+# For Django Email Backend
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'boss.ibragim.magomedov.04@inbox.ru'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')  # os.environ['password_key'] suggested
+EMAIL_USE_TLS = True
